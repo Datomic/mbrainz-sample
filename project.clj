8ad4f7a18b2f13b938338ae69f189f8d751f6b2a
Bobby Calderwood <bobby_calderwood@mac.com>
2013-05-20 16:13:31 -0400

    Tweak project.clj for distribution

--- project.clj
+++ project.clj
@@ -4,7 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.8.3952"]]
+                 [com.datomic/datomic-free "0.8.3960"]
+
+                 ;; Download Pro from http://downloads.datomic.com/pro.html
+                 ;; unzip, then cd into distribution root
+                 ;; Run `bin/maven-install` to install to local maven repository
+                 ;; Then, comment out the free version above, and
+                 ;; enable the pro version below
+                 #_[com.datomic/datomic-pro "0.8.3960"]]
   :source-paths ["src/clj" "examples/clj"]
   :plugins      [[lein-marginalia "0.7.1"]]
-  :jvm-opts ["-Xmx4g" "-server"])
+  :jvm-opts ["-Xmx1g" "-server"])
