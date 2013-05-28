9c0da3625e9121ff941204da12d5bb20b420f9be
Bobby Calderwood <bobby_calderwood@mac.com>
2013-05-28 11:34:57 -0400

    Upgrade datomic to 0.8.3971

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.8.3960"]
+                 [com.datomic/datomic-free "0.8.3971"]
 
                  ;; To run on Datomic Pro, comment out the free
                  ;; version above, and enable the pro version below
-                 #_[com.datomic/datomic-pro "0.8.3960"]]
+                 #_[com.datomic/datomic-pro "0.8.3971"]]
   :source-paths ["src/clj" "examples/clj"]
   :jvm-opts ["-Xmx1g" "-server"])
